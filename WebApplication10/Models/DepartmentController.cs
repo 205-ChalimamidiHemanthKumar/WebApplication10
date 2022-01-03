1922d97e0817efcfd03785948e5b89ba0213e4ae
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-03 17:12:16 +0530

    Add project files.

---
+++ WebApplication10/Models/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication10.Models
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Department> departments = new List<Department>();
+            departments.Add(new Department() { DeptId = 205, DeptName = "IT Department", DeptLocation = "Bangalore" });
+            departments.Add(new Department() { DeptId = 205, DeptName = "Marketing", DeptLocation = "Madanapalle" });
+            departments.Add(new Department() { DeptId = 205, DeptName = "Developing", DeptLocation = "Chennai" });
+
+            ViewData["departments"] = departments;
+            return View();
+        }
+    }
+}
